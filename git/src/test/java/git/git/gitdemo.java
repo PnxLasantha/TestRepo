61b8cf9ca3b4928b8fa7d5528deae6d5a3d98aef
PnxLasantha <dlasanthax@gmail.com>
2017-05-24 00:49:24 +0530

    ddddadad

--- git/src/test/java/git/git/gitdemo.java
+++ git/src/test/java/git/git/gitdemo.java
@@ -4,8 +4,8 @@ public class gitdemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("sssaa");
-		System.out.println("ddddd");	
+			System.out.println("ssdddsaa");
+		System.out.println("ddddddddd");	
 	}
 
 }
